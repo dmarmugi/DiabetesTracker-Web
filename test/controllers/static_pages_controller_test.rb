9f6ff3e40a3fbe74538bade0611ad504507a7646
David Marmugi Laptop <david_marmugi@cable.comcast.com>
2015-08-23 13:24:18 -0400

    RailsTutorial Chapter 3 - Static Pages and ERB
    Modified layout erb and static_pages erbs, and updated test suite to
    test for the right titles.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', 'Home | DiabetesTracker'
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select 'title', 'Help | DiabetesTracker'
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select 'title', 'About | DiabetesTracker'
   end
 end

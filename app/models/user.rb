7d88c4ce89f05f919654a1fe28b1f8b1ed2f1245
David Marmugi Laptop <david_marmugi@cable.comcast.com>
2015-08-24 00:45:05 -0400

    Rails Tutorial Ch. 6 Exercises - Monolithic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :cats
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /.+@.+\..+/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /.+@.+\..+/i },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }

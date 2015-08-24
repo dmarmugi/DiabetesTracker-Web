ff49cd473574db20706732e09904e4e95d82cd90
David Marmugi Laptop <david_marmugi@cable.comcast.com>
2015-08-24 00:40:56 -0400

    RailsTutorial Chapter 6 - Monolithic Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :cats
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /.+@.+\..+/i },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
